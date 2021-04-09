f733bad9c85586bc0080ca5ac11982cb8df0d586
nrd141913 <nrd141913@gmail.com>
2021-04-09 19:44:38 +0600

    ema john server done

---
+++ src/components/ProcessPayment/ProcessPayment.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Elements, CardElement } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import SimpleCardForm from './SimpleCardForm';
+// import SimpleSplitForm from './SimpleSplitForm';
+const stripePromise = loadStripe('pk_test_51IeGjvINoMfSFYtp8AfrETeunUPxqvb5CT9boxhyfKfu1uYqsmECYT4XbLLhYrAmYwV4TayJ2cwZqw6aWFFG9g2S00UcORVD3A');
+
+const processPayment = ({handlePayment}) => {
+    return (
+        <Elements stripe={stripePromise}>
+            <SimpleCardForm handlePayment={handlePayment}></SimpleCardForm>
+        </Elements>
+    );
+};
+
+export default processPayment;
